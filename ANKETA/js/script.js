64c2663fb96d45360684f93065eb23da75d9e4be
Dziyana91 <dziyanaletko@gmail.com>
2021-12-26 02:27:15 +0300

    minor  changes

--- ANKETA/js/script.js
+++ ANKETA/js/script.js
@@ -1,7 +1,7 @@
 let fam = prompt('введите вашу фамилию');
 let name = prompt('введите ваше имя');
 let patr = prompt('введите ваше отчество');
-let age = parseInt( prompt('введите ваш возраст') );            
+let age = parseInt(prompt('введите ваш возраст'));
 let isWoman = confirm('ваш пол - женский?');
 
 let age_days = age * 365;
@@ -12,5 +12,5 @@ let gender = (isWoman == true) ? 'женский' : 'мужской';
 let senior = (age > 65) ? 'да' : 'нет';
 
 alert(
-    `ваше ФИО: ${fam} ${name} ${patr}\nваш возраст в годах: ${age}\nваш возраст в днях: ${age_days}\nчерез 5 лет вам будет: ${age_5}\nваш пол: ${gender}\nвы на пенсии: ${senior}`
+	`ваше ФИО: ${fam} ${name} ${patr}\nваш возраст в годах: ${age}\nваш возраст в днях: ${age_days}\nчерез 5 лет вам будет: ${age_5}\nваш пол: ${gender}\nвы на пенсии: ${senior}`
 );
