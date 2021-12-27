3659829a03f7aafe05da75a9335e48659fd90db1
Dziyana91 <dziyanaletko@gmail.com>
2021-12-27 20:01:32 +0300

    update

--- TREESUM/js/script.js
+++ TREESUM/js/script.js
@@ -1,1 +1,37 @@
 "use strict";
+
+let array = [5, 7,
+	[4, [2], 8, [1, 3], 2],
+	[9, []],
+	1, 8
+];
+
+// let sum = treeSum(array);
+
+console.log(treeSum(array));
+
+
+function treeSum(arr) {
+	let sum = null;
+
+	for (let n = 0; n < arr.length; n++) {
+
+		// if (typeof (arr[n]) == 'number') {
+		// 	sum = sum + arr[n];
+		// } else if (typeof (arr[n]) == 'object') {
+		// 	sum = sum + treeSum(arr[n]);
+		// }
+
+		switch (true) {
+			case typeof (arr[n]) == 'number':
+				sum += arr[n];
+				break;
+			case typeof (arr[n]) == 'object':
+				sum += treeSum(arr[n]);
+				break;
+			default:
+				sum = sum;
+		}
+	}
+	return sum;
+};
