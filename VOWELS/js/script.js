f66b15deb5487fa4d85d82f7210aace5a0c2a68a
Dziyana91 <dziyanaletko@gmail.com>
2022-01-07 23:55:44 +0300

    reviewed under video

--- VOWELS/js/script.js
+++ VOWELS/js/script.js
@@ -33,17 +33,17 @@ function vowelsCount(phrase) {
 // * самый быстро работающий вариант - через if и перебор всех гласных
 
 // можно также с библиотекой в виде массива, но хэш быстрее массива
-function vowelsCount(phrase) {
-	const VOWELS_LIBRARY = ['а', 'я', 'у', 'ю', 'о', 'ё', 'и', 'ы', 'э', 'е',];  //! вместо массова можно было просто записать строкой, строка это массив букв, с ней работают те же методы
-	phrase = phrase.toLowerCase();
-	let count = 0;
+// function vowelsCount(phrase) {
+// 	const VOWELS_LIBRARY = ['а', 'я', 'у', 'ю', 'о', 'ё', 'и', 'ы', 'э', 'е',];  //! вместо массова можно было просто записать строкой, строка это массив букв, с ней работают те же методы
+// 	phrase = phrase.toLowerCase();
+// 	let count = 0;
 
-	for (let i = 0; i < phrase.length; i++) {
-		let charset = phrase[i];						// перебираем буквы в строке по очереди
+// 	for (let i = 0; i < phrase.length; i++) {
+// 		let charset = phrase[i];						// перебираем буквы в строке по очереди
 
-		if (VOWELS_LIBRARY.includes(charset)) {	//! VOWELS_LIBRARY.indexOf(charset)!==-1
-			count++
-		};
-	}
-	return count;
-}
+// 		if (VOWELS_LIBRARY.includes(charset)) {	//! VOWELS_LIBRARY.indexOf(charset)!==-1
+// 			count++
+// 		};
+// 	}
+// 	return count;
+// }
