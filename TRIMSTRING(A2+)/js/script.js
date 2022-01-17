4703ab3d47f6ce85bdf13b059fcdc8733b7f750f
Dziyana91 <dziyanaletko@gmail.com>
2022-01-17 21:49:28 +0300

    A2+ updated

--- TRIMSTRING(A2+)/js/script.js
+++ TRIMSTRING(A2+)/js/script.js
@@ -11,16 +11,32 @@ function stringTrim(s) {
 
 	let spaceCountStart = 0;				// подсчет пробелов с начала
 	let spaceCountEnd = 0;					// подсчет пробелов с конца
+	let sTrimmed = null;
 
 	for (let i = 0; s[i] === ' '; i++) {
 		spaceCountStart++;
+	}
+
+	console.log(spaceCountStart + ' - пробелов в начале строки');
+
+	if (spaceCountStart == sLength) {
+		console.log('строка состоит только из пробелов');
+		sTrimmed = '';
+		return sTrimmed;
 	}
 
 	for (let i = 0; s[lastCharNo - i] === ' '; i++) {
 		spaceCountEnd++;
 	}
 
-	let sTrimmed = s.slice(spaceCountStart, sLength - spaceCountEnd);	// если в конце нет пробелов, то обрезается до +1 несуществующего символа в конце и последний символ не удаляется
+	console.log(spaceCountEnd + ' - пробелов в конце строки');
+
+	if (spaceCountStart == 0 && spaceCountEnd == 0) {
+		console.log('в начале и конце строки нет пробелов');
+		return s;
+	}
+
+	sTrimmed = s.slice(spaceCountStart, sLength - spaceCountEnd);	// если в конце нет пробелов, то обрезается до +1 несуществующего символа в конце и последний символ не удаляется
 
 	return sTrimmed;
 }
