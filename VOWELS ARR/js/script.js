9bb9375afa49469e56adb56328183d89a8b5974f
Dziyana91 <dziyanaletko@gmail.com>
2022-01-18 20:57:07 +0300

    N17 updated

--- VOWELS ARR/js/script.js
+++ VOWELS ARR/js/script.js
@@ -1,7 +1,6 @@
 "use strict";
 
-let phrase = prompt('введите фразу для подсчета в ней гласных букв').toLowerCase();
-let phraseArray = phrase.split('');
+let phrase = prompt('введите фразу для подсчета в ней гласных букв');
 
 const LETTERS_LIBRARY = {
 	'у': 'у',
@@ -16,36 +15,42 @@ const LETTERS_LIBRARY = {
 	'ё': 'ё',
 };
 
-console.log(vowelsCountFE(phraseArray) + ' - с использованием метода массива forEach');
-console.log(vowelsCountF(phraseArray) + ' - с использованием метода массива filter');
-console.log(vowelsCountR(phraseArray) + ' - с использованием метода массива reduce');
+console.log(vowelsCountFE(phrase) + ' - с использованием метода массива forEach');
+console.log(vowelsCountF(phrase) + ' - с использованием метода массива filter');
+console.log(vowelsCountR(phrase) + ' - с использованием метода массива reduce');
 
 function vowelsCountFE(phrase) {
+	let phraseLC = phrase.toLowerCase();
+	let phraseArray = phraseLC.split('');
 	let count = 0;
 	function countingForEach(value, i, array) {
 		if (value in LETTERS_LIBRARY) {
 			count++;
 		}
 	}
-	phrase.forEach(countingForEach)
+	phraseArray.forEach(countingForEach)
 	return count;
 }
 
 function vowelsCountF(phrase) {
+	let phraseLC = phrase.toLowerCase();
+	let phraseArray = phraseLC.split('');
 	function countingFilter(value, i, array) {
 		return (value in LETTERS_LIBRARY);
 	}
-	let vowelsArray = phrase.filter(countingFilter);
+	let vowelsArray = phraseArray.filter(countingFilter);
 	return vowelsArray.length;
 }
 
 function vowelsCountR(phrase) {
+	let phraseLC = phrase.toLowerCase();
+	let phraseArray = phraseLC.split('');
 	function countingReduce(r, value, i, array) {
 		if (value in LETTERS_LIBRARY) {
 			r++;
 		}
 		return r;
 	};
-	let count = phrase.reduce(countingReduce, 0);
+	let count = phraseArray.reduce(countingReduce, 0);
 	return count;
 }
